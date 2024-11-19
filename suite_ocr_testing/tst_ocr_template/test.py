686c0629ffbf4819ef520f43da845d6b3bd499b1
KayeJD <kjdumalag@gmail.com>
2024-11-19 19:45:36 +0000

    Create OCR testing template + example file
    TG-178
    Create tst_ocr_template file in new suite_ocr_testing test suite
    to demostrate working OCR engine

--- suite_ocr_testing/tst_ocr_template/test.py
+++ suite_ocr_testing/tst_ocr_template/test.py
@@ -2,28 +2,58 @@
 
 import names
 
-def setUp():
-    testSettings.logScreenshotOnError = True
-    testSettings.logScreenshotOnFail = True
-    testSettings.logScreenshotOnWarning = True
-    testSettings.logScreenshotOnPass = True
-    testSettings.reportFormat = "html"  # or "xml" for XML reports
-    testSettings.reportDir = "/home/kadum"
+def registerAUT(aut, path, squishserver_host=None, squishserver_port=None):
+    s = '"' + os.environ["SQUISH_PREFIX"] + '/bin/squishrunner"'
+    if squishserver_host is not None:
+        s += ' --host ' + squishserver_host
+    if squishserver_port is not None:
+        s += ' port=' + str(squishserver_port)
+    s += ' --config addAUT "' + aut + '" "' + path + '"'
+    if sys.platform == "win32" and s.endswith('"'):
+        s = '"' + s
+    test.log("Executing: " + s)
+    os.system(s)
+    
+
+def save_screenshot(filename):
+    # widget = waitForObject(obj_name)
+    # img = object.grabScreenshot(widget)
+    img.save(filename)
+    testData.get(filename)
+    
+    screenshot_dir = '/home/kadum/GDMS-Capstone/suite_ocr_testing/tst_screenshotting'
+    if not os.path.exists(screenshot_dir):
+        os.makedirs(screenshot_dir)
+    
+    full_path = os.path.join(screenshot_dir, filename)
+    img.save(full_path)
+    testData.get(full_path)
+
+
+def findText(text):
+    if (test.ocrTextPresent(text)):
+        test.passes("PASS: " + text + " text visible upon scollView")
+    elif ((test.ocrTextPresent(text)) == False):
+        test.fail("FAIL: " + text + "text not visible on screen")
+        screenshot_name = f"{text}_ocrfail"
+        save_screenshot(screenshot_name)
+    else:
+        test.log(f"Failed to perform findText test on {text}")
+        
+    
     
 def runTestRecording():
     startApplication("ThermostatApp")
-    mouseClick(waitForObject(names.scrollView_toggle_CustomSwitch_2), 38, 4, Qt.LeftButton)
-    mouseClick(waitForObject(names.scrollView_background_Rectangle), 465, 20, Qt.LeftButton)
-    mouseClick(waitForObject(names.thermostat_MultiEffect_2), 14, 19, Qt.LeftButton)
+    mouseWheel(waitForObject(names.scrollView_toggle_CustomSwitch), 19, 35, 0, -30, Qt.NoModifier)
+    mouseWheel(waitForObject(names.scrollView_Flickable), 494, 57, 0, -735, Qt.NoModifier)
     
-    # Testing to see 'Thermostat Control' in page 2
-    if test.ocrTextPresent("Thermostat Control"):
-        test.log("Page 2 displays correct heading value")
-    else:
-        test.log("Page 2 displays incorrect heading value")
+    # Insert ocr text verification, and add logic to create the test pass/fail criteria
+    findText("Kitchen") # can toggle for testing
+    findText("Bedroom")
+    # findText("Thermostat")
     
     closeWindow(names.thermostat_QQuickWindowQmlImpl)
 
+
 def main():
-    setUp()
     runTestRecording()
